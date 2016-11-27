239d17992142a4aaf239e3c668f01cdc8ad5bb84
jennyzeng <jennyzengzzh@gmail.com>
2016-11-27 06:18:13 -0500

    add explanations for methods in testFunc

--- testFunc.py
+++ testFunc.py
@@ -1,5 +1,9 @@
-import platform
+"""
+@author: Jenny Zeng zhaohuaz@uci.edu
+this is a file for removing boundary of a image
+"""
 
+import platform
 import numpy as np
 import shutil
 from PIL import Image
@@ -8,96 +12,112 @@ from scipy.misc import toimage
 import matplotlib.pyplot as plt
 import os
 
-def convertMatHelper( filename):
-    img = Image.open(filename).convert('I')
 
-    # img = img.resize((self.width,self.height),Image.ANTIALIAS)  #resize
-    data = np.array(img)  # .reshape(self.width*self.height,1).T
-    # for thresholding
-    low_values_indices = data < 180
-    data[low_values_indices] = 0
-    high_values_indices = data >= 180
-    data[high_values_indices] = 1
-    # toimage(data).show()
-    return data
+def convertMatHelper(filename):
+	"""
+	open an image, make it a 2-d array and normalize to 0 and 1
+	:param filename: image location
+	:return: a 2-d array
+	"""
+	img = Image.open(filename).convert('I')
+
+	# img = img.resize((self.width,self.height),Image.ANTIALIAS)  #resize
+	data = np.array(img)  # .reshape(self.width*self.height,1).T
+	# for thresholding and mormalize
+	low_values_indices = data < 180
+	data[low_values_indices] = 0
+	high_values_indices = data >= 180
+	data[high_values_indices] = 1
+	# toimage(data).show()
+	return data
+
 
 def constructPointsArray(data):
-    points = []
-    for i in range(len(data)):
-        for j in range(len(data[i])):
-            if data[i][j] == 0:
-                points.append([i, j])
-    return np.array(points)
+	"""
+	construct a array in which each list is a point representing
+	the coordinates of the pixels that are black (0)
+	:param data: 2-d array
+	:return: a array in which each list is a point representing
+	the coordinates of the pixels that are black (0)
+	"""
+	points = []
+	for i in range(len(data)):
+		for j in range(len(data[i])):
+			if data[i][j] == 0:
+				points.append([i, j])
+	return np.array(points)
 
 
 def findBoundary(data, points):
-    hull = ConvexHull(points)
-    plt.plot(points[:, 0], points[:, 1], 'o')
-    # print "attention: ", len(data[0])
-    for simplex in hull.simplices:
-        plt.plot(points[simplex, 0], points[simplex, 1], 'k-')
-        p1 = points[simplex]
-        p2 = points[simplex]
+	"""
+	use convex hull algorithm to find the boundary of the image
+	and make the pixels that are used to construct the polygon in convex hull.
+	:param data: 2 d array of image
+	:param points: corresponding coordinates of the pixels that are black (0)
+	:return: modified 2-d array
+	"""
+	hull = ConvexHull(points)
+	plt.plot(points[:, 0], points[:, 1], 'o')
+	for simplex in hull.simplices:
+		plt.plot(points[simplex, 0], points[simplex, 1], 'k-')
+		p1 = points[simplex]
+		p2 = points[simplex]
 
-        data[p1[0][0]][p1[0][1]]= 1
-        data[p1[1][0]][p1[1][1]]= 1
-        data[p2[0][0]][p2[0][1]] = 1
-        data[p2[1][0]][p2[1][1]]= 1
+		data[p1[0][0]][p1[0][1]] = 1
+		data[p1[1][0]][p1[1][1]] = 1
+		data[p2[0][0]][p2[0][1]] = 1
+		data[p2[1][0]][p2[1][1]] = 1
+	### display the convexHull result
+	# toimage(data).show()
+	# plt.show()
 
-    # toimage(data).show()
-    # plt.show()
-
-    hull.close()
-    return data
-
-def processImg(filename, destination, toFormat= ".jpg", ):
-    # destination = resultFolder+ fileDirectory + filename +"-dir/"
-    # if not os.path.exists(destination):
-    #     os.makedirs(destination)
-    data = convertMatHelper(destination + filename)
-    # area = (len(data)-1) * (len(data[0])-1)
-    # print ("my area: ", area)
-    i = 0
-    while i<25:
-        points = constructPointsArray(data)
-        data= findBoundary(data, points)
-        toimage(data).save(destination + filename+"-" + str(i) + toFormat)
-        plt.savefig(destination + filename+"-plt-"+str(i)+ toFormat)
-        # toimage(data).save(resultFolder + fileDirectory + filename+"-" + str(i) + toFormat)
-        # plt.savefig(resultFolder+fileDirectory+filename+"-plt-"+str(i)+ toFormat)
-        plt.close()
-        i+=1
+	hull.close()
+	return data
 
 
-def main(originalFolder = "./test/",resultFolder = "./convertedImges/"):
-    if os.path.exists(resultFolder):
-        shutil.rmtree(resultFolder)
-    for subDir in os.listdir(originalFolder):
-        for img in os.listdir(originalFolder+subDir):
-            destination = resultFolder + subDir +"/"+ img + "-dir/"
-            print destination
-            print img
-            if not os.path.exists(destination):
-                os.makedirs(destination)
-            shutil.copy(originalFolder +"/"+ subDir + "/" + img, destination)
-            processImg( img, destination)
+def processImg(filename, destination, toFormat=".jpg", times=25):
+	"""
+	remove boundary in a image for times
+	:param filename: the image location
+	:param destination: where the modified image will be stored
+	:param toFormat: the result format of the image
+	:param times: times of running removing boundary process
+	:return: None
+	"""
+	data = convertMatHelper(destination + filename)
+	i = 0
+	while i < times:
+		points = constructPointsArray(data)
+		findBoundary(data, points)
+		toimage(data).save(destination + filename + "-" + str(i) + toFormat)
+		plt.savefig(destination + filename + "-plt-" + str(i) + toFormat)
+		plt.close()
+		i += 1
 
+
+def main(originalFolder="./test/", resultFolder="./convertedImges/"):
+	"""
+	process all the images in the original folder and store them into the
+	result folder.
+	:param originalFolder: where original images from
+	:param resultFolder: where the modified images will be stored
+	:return: None
+	"""
+	if os.path.exists(resultFolder):
+		shutil.rmtree(resultFolder)
+	for subDir in os.listdir(originalFolder):
+		for img in os.listdir(originalFolder + subDir):
+			destination = resultFolder + subDir + "/" + img + "-dir/"
+			print "current destination: ", destination
+			print "current image: ", img
+			if not os.path.exists(destination):
+				os.makedirs(destination)
+			shutil.copy(originalFolder + "/" + subDir + "/" + img, destination)
+			processImg(img, destination)
 
 
 if __name__ == '__main__':
-    if(platform.system()== "Darwin"):
-        os.system("find . -name '.DS_Store' -type f -delete")
-    # data = convertMatHelper("test/3/Nm.png")
-    # print data
-    # print len(data)
-    # print data
-    # points = np.random.rand(30, 2)  # 30 random points in 2-D
-
-    # print points
-    # a = constructPointsArray(data)
-    # print "a: ", a
-    # print "a end"
-
-    # findBoundary(data, a)
-    # process("Po1")
-    main()
+	# to remove the .DS_Store file on mac
+	if (platform.system() == "Darwin"):
+		os.system("find . -name '.DS_Store' -type f -delete")
+	main()
